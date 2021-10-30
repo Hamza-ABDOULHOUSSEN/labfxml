a88f0cd05d5846032867996a8b4b3dd27c1300d8
ABDOULHOUSSEN Hamza <hamza.abdoulhoussen@telecomnancy.eu>
2021-10-30 14:35:09 +0200

    Insertion panneau de droite

---
+++ Album/src/main/java/album/Controller/DroiteInventairePhotos.java
@@ -0,0 +1,15 @@
+package album.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class DroiteInventairePhotos {
+
+    @FXML
+    private GridPane inventaire_photos;
+
+    @FXML
+    protected void AddPhotoClick() {
+        System.out.println("Le bouton fonctionne");
+    }
+}
