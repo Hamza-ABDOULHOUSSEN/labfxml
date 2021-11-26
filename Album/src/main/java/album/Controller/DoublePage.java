f70fe365e06c503e2acef656ec66bbe301a5762d
ABDOULHOUSSEN Hamza <hamza.abdoulhoussen@telecomnancy.eu>
2021-11-26 21:01:53 +0100

    Changement titre

--- Album/src/main/java/album/Controller/DoublePage.java
+++ Album/src/main/java/album/Controller/DoublePage.java
@@ -4,17 +4,33 @@ import album.Observateur.Observateur;
 import album.model.Album;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
 
 public class DoublePage implements Observateur {
 
     Album album;
 
     @FXML
+    VBox vbox1;
+    @FXML
+    VBox vbox2;
+
+    @FXML
     Button title1;
+    TextField tf1 = new TextField();
+    @FXML
+    Pane pane1;
 
     @FXML
     Button title2;
+    TextField tf2 = new TextField();
+    @FXML
+    Pane pane2;
 
     public DoublePage(Album album) {
         this.album = album;
@@ -22,13 +38,41 @@ public class DoublePage implements Observateur {
     }
 
     @FXML
-    protected void title1Click() {}
+    protected void title1Click() {
+        vbox1.getChildren().clear();
+        tf1.setOnAction(e -> {
+            String titre = tf1.getText();
+            tf1.clear();
+            album.changetitle(titre, 1);
+        });
+        vbox1.getChildren().addAll(tf1, pane1);
+    }
 
     @FXML
-    protected void title2Click() {}
+    protected void title2Click() {
+        vbox2.getChildren().clear();
+        tf2.setOnAction(e -> {
+            String titre = tf2.getText();
+            tf2.clear();
+            album.changetitle(titre, 2);
+        });
+        vbox2.getChildren().addAll(tf2, pane2);
+    }
 
     @Override
     public void update() {
+        ArrayList<String> doublepage_titre = album.getDoublepage_titre();
+        ArrayList<ImageView> doublepage_image = album.getDoublepage_image();
+        int pos = album.getDouble_page_courante();
 
+        title1.setText(doublepage_titre.get(pos-1));
+        title2.setText(doublepage_titre.get(pos));
+        
+        vbox1.getChildren().clear();
+        vbox1.getChildren().addAll(title1, pane1);
+        vbox2.getChildren().clear();
+        vbox2.getChildren().addAll(title2, pane2);
+        
+                
     }
 }
--- Album/src/main/java/album/model/Album.java
+++ Album/src/main/java/album/model/Album.java
@@ -7,14 +7,21 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Album extends SujetObserve {
     
     // Liste: observateurs = {root , savepane, askimagepane, droiteinventairephotos}
     // pour avoir acces aux observateurs et ne pas tous les modifier à chaque fois
 
     String nom_album = "";
+
     int nb_double_page = 2;
     int double_page_courante = 1;
+
+    ArrayList<String> doublepage_titre = new ArrayList<String>(Arrays.asList("titre", "titre"));
+    ArrayList<ImageView> doublepage_image = new ArrayList<ImageView>(Arrays.asList(new ImageView(), new ImageView()));
     
     public Album() {}
 
@@ -35,12 +42,20 @@ public class Album extends SujetObserve {
 
     public void ajouter_double_page() {
         nb_double_page+=2;
+        doublepage_titre.add("titre");
+        doublepage_titre.add("titre");
+        doublepage_image.add(new ImageView());
+        doublepage_image.add(new ImageView());
         notifierObservateurs();
     }
 
     public void retirer_double_page() {
         if (nb_double_page != 2) {
             nb_double_page-=2;
+            doublepage_titre.remove(double_page_courante-1);
+            doublepage_titre.remove(double_page_courante);
+            doublepage_image.remove(double_page_courante-1);
+            doublepage_image.remove(double_page_courante);
         }
         notifierObservateurs();
     }
@@ -59,10 +74,26 @@ public class Album extends SujetObserve {
         notifierObservateurs();
     }
 
+    public void changetitle(String titre, int page) {
+        int pos;
+        if (page==1) {
+            pos = double_page_courante - 1;
+        }
+        else {
+            pos = double_page_courante;
+        }
+
+        doublepage_titre.set(pos, titre);
+
+        notifierObservateurs();
+
+    }
+
 
     // GETTERS
     public String getNom_album() {return this.nom_album;}
     public int getNb_double_page() {return this.nb_double_page;}
     public int getDouble_page_courante() {return this.double_page_courante;}
-    
+    public ArrayList<String> getDoublepage_titre() {return this.doublepage_titre;}
+    public ArrayList<ImageView> getDoublepage_image() {return this.doublepage_image;}
 }
