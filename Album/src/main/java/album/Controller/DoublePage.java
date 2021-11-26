9a5597826d6a10029cd8e8d222e3255e1b0f55bb
ABDOULHOUSSEN Hamza <hamza.abdoulhoussen@telecomnancy.eu>
2021-11-26 07:02:01 +0100

    Titre en Button + setStyle

--- Album/src/main/java/album/Controller/DoublePage.java
+++ Album/src/main/java/album/Controller/DoublePage.java
@@ -3,15 +3,29 @@ package album.Controller;
 import album.Observateur.Observateur;
 import album.model.Album;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
 public class DoublePage implements Observateur {
 
     Album album;
 
+    @FXML
+    Button title1;
+
+    @FXML
+    Button title2;
+
     public DoublePage(Album album) {
         this.album = album;
         album.ajouterObservateur(this);
     }
+
+    @FXML
+    protected void title1Click() {}
+
+    @FXML
+    protected void title2Click() {}
 
     @Override
     public void update() {
